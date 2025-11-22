8fb66b3f99ee9d0589779ff26e691a802ac53e90
Malith Anjana <malithanjana97@gmail.com>
2025-11-22 09:32:48 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <section>
+          <h1 className="text-center">The Hub for Every Dev <br/> Event You Can&#39;t Miss</h1>
+          <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+
+      </section>
+    )
+    }

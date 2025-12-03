2aa9f78f4c91647cc27244b064c9175bd206dd4a
Malith Anjana <malithanjana97@gmail.com>
2025-12-03 14:26:04 +0530

    implement fetch  all events

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -46,3 +46,15 @@ export async function POST(req: NextRequest) {
             {status: 400});
     }
 }
+
+export async function GET() {
+    try {
+        await connectDB();
+
+        const events = await Event.find().sort({createdAt: -1});
+
+        return NextResponse.json({message: "Events fetched successfully", events}, {status: 200});
+    } catch (e) {
+        return NextResponse.json({message: "Events fetching failed", error: e}, {status: 400});
+    }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
-import {events} from "@/lib/constants";
+import {IEvent} from "@/database";
 
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Page = async () => {
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const {events} = await response.json();
+    console.log(events)
+
     return (
         <section>
             <h1 className="text-center">The Hub for Every Dev <br/> Event You Can&#39;t Miss</h1>
@@ -11,7 +17,7 @@ const Page = () => {
             <div className="mt-20 space-y-7">
                 <h3>Featured Events</h3>
                 <ul className="events">
-                    {events.map((event) => (
+                    {events && events.length > 0 && events.map((event: IEvent) => (
                         <li key={event.title}>
                             <EventCard {...event}/>
                         </li>
